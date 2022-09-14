40c81baa533f755cf187ae39e9244e0d6d6d7d29
Samuel V <samuelveiga@edu.unifor.br>
2022-09-14 01:49:42 -0300

    melhorias interface

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import MinhasCompras
+from .views import MinhasCompras, Perfil
 
 app_name = "users"
 
 urlpatterns = [
     path("minhas-compras/", MinhasCompras.as_view(), name="minhas_compras"),
+    path('dados/<int:pk>/', Perfil.as_view(), name='perfil')
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
-from django.views.generic import ListView
 from produtos.models import Ordem
+from .models import  CustomUser
 
+from django.views.generic import ListView, UpdateView
+from django.contrib.auth.mixins import  LoginRequiredMixin
 
-class MinhasCompras(ListView):
+class MinhasCompras(LoginRequiredMixin, ListView):
     model = Ordem
     context_object_name = "ordens"
     template_name = "users/minhas_compras.html"
@@ -11,3 +12,8 @@ class MinhasCompras(ListView):
     def get_queryset(self):
         qs = super().get_queryset()
         return qs.filter(user=self.request.user)
+
+class Perfil(LoginRequiredMixin, UpdateView):
+    model = CustomUser
+    template_name = 'users/perfil.html'
+    fields = ['username', 'first_name', 'last_name', 'email']
