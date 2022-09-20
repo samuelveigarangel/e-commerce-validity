c8eb07ca6f117a18ab832821e6a786f00e0e024f
Samuel V <samuelveiga@edu.unifor.br>
2022-09-20 03:17:07 -0300

    add heroku settings

--- setup/settings/prod.py
+++ setup/settings/prod.py
@@ -1,8 +1,6 @@
 import django_on_heroku
 from decouple import config
 
-from setup.settings.dev import ALLOWED_HOSTS
-
 from .base import *
 
 SECRET_KEY = config("SECRET_KEY")
@@ -12,3 +10,35 @@ DEBUG = False
 ALLOWED_HOSTS = [
     "e-commerce-validity.herokuapp.com",
 ]
+
+DEBUG_PROPAGATE_EXPECTIONS = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'MYAPP': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
+#Heroku Settings
+django_on_heroku.settings(locals(), staticfiles=False)
+del DATABASES['default']['OPTIONS']['sslmode']
