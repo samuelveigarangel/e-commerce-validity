05c9f26378fe7321ab369f5aea6709b333fd8e99
Samuel V <samuelveiga@edu.unifor.br>
2022-09-21 10:13:37 -0300

    config StorageMedia S3

--- setup/settings/prod.py
+++ setup/settings/prod.py
@@ -32,7 +32,7 @@ AWS_QUERYSTRING_AUTH = False
 
 AWS_HEADERS = {'Access-Control-Allow-Origin': '*'}
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
