a43f1326ac4411de22688457784c3d83066fa0bb
Samuel V <samuelveiga@edu.unifor.br>
2022-09-21 10:59:52 -0300

    configuração media e static bucket s3

--- setup/settings/storage_backends.py
+++ setup/settings/storage_backends.py
@@ -1,5 +1,11 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class MediaStorage(S3Boto3Storage):
+    bucket_name = 'e-commerce--assets'
     location = 'media'
+    custom_domain = '{}.s3.amazonaws.com'.format(bucket_name)
     file_overwrite = False
+
+class StaticStorage(S3Boto3Storage):
+    bucket_name = 'e-commerce-static'
+    location = 'static'
