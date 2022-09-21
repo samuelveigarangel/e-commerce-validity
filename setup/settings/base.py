63b35dd4c7b1b0b5dd8f181aa9c68dcfb61ff6e0
Samuel V <samuelveiga@edu.unifor.br>
2022-09-21 09:55:18 -0300

    fixed

--- setup/settings/base.py
+++ setup/settings/base.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # MEDIA_URL = "/media/"
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
